da100cf11f9b831c6035962ebad2283cc204223d
wendelca <vvkamalov_obs@mail.ru>
2025-03-20 19:19:03 +0500

    feat(email-notif): add config class with consumer factory and listener, change trusted package property in applicaton.yml

---
+++ modules/email-notification-service/src/main/java/com/github/tennyros/emailnotificationservice/config/KafkaConfig.java
@@ -0,0 +1,33 @@
+package com.github.tennyros.emailnotificationservice.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.Map;
+
+@Slf4j
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+
+    @Bean
+    public ConsumerFactory<String, Object> consumerFactory(KafkaProperties kafkaProperties) {
+        Map<String, Object> conf = kafkaProperties.buildConsumerProperties();
+        return new DefaultKafkaConsumerFactory<>(conf);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
+            ConsumerFactory<String, Object> consumerFactory) {
+        ConcurrentKafkaListenerContainerFactory<String, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory);
+
+        return factory;
+    }
+}
