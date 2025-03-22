aac294304360e21ef986f1b0fef988b3da5c146b
wendelca <vvkamalov_obs@mail.ru>
2025-03-22 12:21:00 +0500

    feat(idempotent-consumer): add idepmotent consumer logic to existing consumer
    - add producer record in product-service with message id header to set it in send result for kafka template;
    - add data jpa starter and h2 db to save message id for idempotence logic;
    - add in handle method of email notif service idempotent consumer logic with trancation to handle message id existance in db.

---
+++ modules/email-notification-service/src/main/java/com/github/tennyros/emailnotificationservice/persistence/entity/ProcessedEventEntity.java
@@ -0,0 +1,33 @@
+package com.github.tennyros.emailnotificationservice.persistence.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@Table(name = "processed_events")
+public class ProcessedEventEntity {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String messageId;
+
+    @Column(nullable = false)
+    private String productId;
+
+    public ProcessedEventEntity(String messageId, String productId) {
+        this.messageId = messageId;
+        this.productId = productId;
+    }
+}
---
+++ modules/email-notification-service/src/main/java/com/github/tennyros/emailnotificationservice/persistence/repository/ProcessedEventRepository.java
@@ -0,0 +1,11 @@
+package com.github.tennyros.emailnotificationservice.persistence.repository;
+
+import com.github.tennyros.emailnotificationservice.persistence.entity.ProcessedEventEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProcessedEventRepository extends JpaRepository<ProcessedEventEntity, Long> {
+
+    ProcessedEventEntity findByMessageId(String messageId);
+}
