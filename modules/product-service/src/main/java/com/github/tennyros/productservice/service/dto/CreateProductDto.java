45254df5378aa5d13e1a6be32766d60219a05703
Vadim Kamalov <vvkamalov_obs@mail.ru>
2025-03-25 10:14:10 +0500

    ref(product-service): refactor kafka producer config in product-service (applicaton.yml, KafkaConfig, KafkaPropertiesConfig, CreateProductDto) (#6)
    **What was done:**
    
    - refactored `KafkaConfig` to use `@EnableConfigurationProperties` and injected `KafkaPropertiesConfig` for better configuration handling;
    - updated `application.yml` to separate Kafka producer properties and custom topic settings under `app.kafka`;
    - modified `KafkaPropertiesConfig` to map topic names, partitions, and replicas from `application.yml`;
    - added `@Builder` to `CreateProductDto` to improve readability and simplify object creation in tests and services.
    
    **Why this is needed:**
    - separating Kafka topic configuration allows easier topic management and reusability across different services;
    - `@EnableConfigurationProperties` ensures proper binding of custom configurations without manual bean initialization;
    - `@Builder` in `CreateProductDto` improves test readability and reduces boilerplate code during object creation.
    
    **How to test:**
    - run the product-service with the updated `application.yml` configuration;
    - ensure the product-created-events-topic is created with the correct number of partitions and replicas;
    - verify product creation produces an event to Kafka without serialization errors;
    - ensure that already existing logic with product-service, mock-service and product-service is working correctly.
    
    **Links:**
     Jira ticket:[KM-6](https://tennyros.atlassian.net/browse/KM-6)

--- modules/product-service/src/main/java/com/github/tennyros/productservice/service/dto/CreateProductDto.java
+++ modules/product-service/src/main/java/com/github/tennyros/productservice/service/dto/CreateProductDto.java
@@ -1,6 +1,7 @@
 package com.github.tennyros.productservice.service.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 
 @Getter
 @Setter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class CreateProductDto {
